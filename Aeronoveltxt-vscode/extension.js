734715fc9b720e1cabc40628cceb04efe3f335ec
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2020-05-21 12:22:39 +0800

    插件update

--- Aeronoveltxt-vscode/extension.js
+++ Aeronoveltxt-vscode/extension.js
@@ -7,6 +7,11 @@ function activate(context) {
     const text = editor.document.getText(editor.selection);
     vscode.env.openExternal(vscode.Uri.parse('https://www.google.com/search?q='+text));
   }));
+  context.subscriptions.push(vscode.commands.registerCommand('aeronoveltxt.searchp1', () => {
+    const editor = vscode.window.activeTextEditor;
+    const text = editor.document.getText(editor.selection);
+    vscode.env.openExternal(vscode.Uri.parse('https://www.google.com/search?q='+text+' 意思'));
+  }));
   context.subscriptions.push(vscode.commands.registerCommand('aeronoveltxt.weblio', () => {
     const editor = vscode.window.activeTextEditor;
     const text = editor.document.getText(editor.selection);
