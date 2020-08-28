e28470a38282292fed53dabb49d51e19d921cd99
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2020-08-29 00:18:58 +0800

    Update publish.bat

--- AeroNovelTool/publish.bat
+++ AeroNovelTool/publish.bat
@@ -2,6 +2,8 @@ dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=true /p:PublishTrimm
 rd /Q /S app
 md app
 move bin\release\netcoreapp3.1\win10-x64\publish\AeroNovelTool.exe app\AeroNovelTool.exe
+del template.zip
+"C:\Program Files\7-Zip\7z.exe" a template.zip ".\template\*"
 copy template.zip app\template.zip
 copy template_bat\drop_atxt_atxt2bbcode.txt app\drop_atxt_atxt2bbcode.bat
 copy template_bat\drop_epub_epub2comment.txt app\drop_epub_epub2comment.bat
