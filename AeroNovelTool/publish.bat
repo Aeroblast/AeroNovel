12521d843a73b9909d8f5d90d776553b06b1eb29
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2020-08-29 00:16:23 +0800

    项目

--- AeroNovelTool/publish.bat
+++ AeroNovelTool/publish.bat
@@ -1,2 +1,10 @@
 dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+rd /Q /S app
+md app
+move bin\release\netcoreapp3.1\win10-x64\publish\AeroNovelTool.exe app\AeroNovelTool.exe
+copy template.zip app\template.zip
+copy template_bat\drop_atxt_atxt2bbcode.txt app\drop_atxt_atxt2bbcode.bat
+copy template_bat\drop_epub_epub2comment.txt app\drop_epub_epub2comment.bat
+copy template_bat\drop_folder_bbcode.txt app\drop_folder_bbcode.bat
+copy template_bat\drop_folder_epub.txt app\drop_folder_epub.bat
 pause
