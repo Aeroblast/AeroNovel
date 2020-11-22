558484cdd971ae9929ebea4ee0863c4e3772810b
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2020-11-22 22:01:20 +0800

    publish settings

--- AeroNovelTool/publish.bat
+++ AeroNovelTool/publish.bat
@@ -1,7 +1,7 @@
-dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish -p:Configuration=Release -r win10-x64
 rd /Q /S app
 md app
-move bin\release\netcoreapp3.1\win10-x64\publish\AeroNovelTool.exe app\AeroNovelTool.exe
+move bin\release\net5\win10-x64\publish\AeroNovelTool.exe app\AeroNovelTool.exe
 del template.zip
 "C:\Program Files\7-Zip\7z.exe" a template.zip ".\template\*"
 copy template.zip app\template.zip
@@ -9,4 +9,5 @@ copy template_bat\drop_atxt_atxt2bbcode.txt app\drop_atxt_atxt2bbcode.bat
 copy template_bat\drop_epub_epub2comment.txt app\drop_epub_epub2comment.bat
 copy template_bat\drop_folder_bbcode.txt app\drop_folder_bbcode.bat
 copy template_bat\drop_folder_epub.txt app\drop_folder_epub.bat
+copy template_bat\drop_epub_epub2comment_with_magic.txt app\drop_epub_epub2comment_with_magic.bat
 pause
