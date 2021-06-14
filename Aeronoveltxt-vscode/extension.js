e92f35ea7e032f07482780ff6a17d9b6fe29ec66
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2021-06-14 14:53:08 +0800

    改进

--- Aeronoveltxt-vscode/extension.js
+++ Aeronoveltxt-vscode/extension.js
@@ -18,18 +18,36 @@ function activate(context) {
     vscode.env.openExternal(vscode.Uri.parse('https://www.weblio.jp/content/' + text));
   }));
   context.subscriptions.push(vscode.commands.registerTextEditorCommand('aeronoveltxt.onDownArrow', (editor, edit, args) => {
-    const position = editor.selection.active;
-    let line = position.line;
-    let testText;
-    do {
-      line++;
+    vscode.commands.executeCommand('cursorMove', {
+      to: "down",
+      by: "wrappedLine"
+    }).then(() => {
+      const position = editor.selection.active;
+      let line = position.line;
       let testRange = new vscode.Range(position.with(line, 0), position.with(line, 2));
-      testText = editor.document.getText(testRange);
-    } while (testText == "##" && line < editor.document.lineCount);
-    let pos = position.with(line, position.character);
-    let sel = new vscode.Selection(pos, pos);
-    editor.selection = sel;
-    editor.revealRange(sel.with(), 1);//1=TextEditorRevealType.InCenter
+      let testText = editor.document.getText(testRange);
+      while (testText == "##" && line < editor.document.lineCount) {
+        line++
+        testRange = new vscode.Range(position.with(line, 0), position.with(line, 2));
+        testText = editor.document.getText(testRange);
+      }
+      if (line != position.line) {
+        let pos = position.with(line, position.character);
+        switch (testText) {
+          case "「」":
+          case "『』":
+          case "（）":
+          case "〔〕":
+            let raw = editor.document.getText(new vscode.Range(position.with(line - 1, 2), position.with(line - 1, 4)));
+            if (raw[0] == testText[0]) { pos = pos.with(pos.line, 1); }
+            break;
+        }
+        let sel = new vscode.Selection(pos, pos);
+        editor.selection = sel;
+        editor.revealRange(sel.with(), 1);//1=TextEditorRevealType.InCenter
+      }
+    });
+
   }));
 }
 exports.activate = activate;
